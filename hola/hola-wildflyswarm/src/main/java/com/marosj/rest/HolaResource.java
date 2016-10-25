89a4bd8dd267965d7103793e3e12d63014794fa0
marosj <marosjgithub@gmail.com>
2016-10-25 11:28:42 +0200

    Get setting from file (or env)

--- hola/hola-wildflyswarm/src/main/java/com/marosj/rest/HolaResource.java
+++ hola/hola-wildflyswarm/src/main/java/com/marosj/rest/HolaResource.java
@@ -1,16 +1,23 @@
 package com.marosj.rest;
 
-import javax.ws.rs.Path;
-import javax.ws.rs.core.Response;
+import org.apache.deltaspike.core.api.config.ConfigProperty;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
 
 @Path("/api/hola")
 public class HolaResource {
 
-	@GET
-	@Produces("text/plain")
-	public Response doGet() {
-		return Response.ok("method doGet invoked").build();
-	}
+    @Inject
+    @ConfigProperty(name = "WF_SWARM_SAYING", defaultValue = "Hola")
+    private String saying;
+
+    @GET
+    @Produces("text/plain")
+    public Response doGet() {
+        return Response.ok(saying + " from WF Swarm").build();
+    }
 }
